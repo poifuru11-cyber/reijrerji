974fee4d3779d8ed901dbdc541ce720c3b9c4fa0
poifuru11-cyber <poifuru11@gmail.com>
2026-03-23 12:09:25 +0900

    Update sales.js

--- sales.js
+++ sales.js
@@ -1,39 +1,26 @@
 function loadSales(){
-
 let sales = JSON.parse(localStorage.getItem("sales")) || []
-
 let html=""
-
 sales.forEach(s=>{
-
 html += `
 <hr>
 ${s.time}<br>
 `
-
 for(let item in s.cart){
-
 html += `${item} × ${s.cart[item].count}<br>`
-
 }
-
 html += `
 合計：${s.total}円<br>
 預り金：${s.payment}円<br>
 お釣り：${s.change}円<br>
 `
-  
 })
-
 document.getElementById("sales").innerHTML = html
-
 }
-
-loadSales()
-
 function clearSales(){
 if(confirm("売上履歴を全削除しますか？")){
 localStorage.removeItem("sales")
 loadSales()
 }
 }
+loadSales()
